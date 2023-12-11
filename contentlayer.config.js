57340edc7612a3d9a3826b942c8ff45745b4cac2
aitikirala <aitikira@terpmail.umd.edu>
2023-12-11 16:25:14 -0500

    tried changing project path

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -4,99 +4,31 @@ import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
-/** @type {import('contentlayer/source-files').ComputedFields} */
-const computedFields = {
-	path: {
-		type: "string",
-		resolve: (doc) => `/${doc._raw.flattenedPath}`,
-	},
-	slug: {
-		type: "string",
-		resolve: (doc) => doc._raw.flattenedPath.split("/").slice(1).join("/"),
-	},
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+
+const fetchRepos = async () => {
+  const response = await fetch('https://api.github.com/users/aitikirala/repos');
+  const repos = await response.json();
+
+  repos.forEach(repo => {
+    const content = `---
+title: "${repo.name}"
+description: "${repo.description}"
+url: "${repo.html_url}"
+repository: "${repo.full_name}"
+published: true
+date: "${repo.created_at}"
+---
+
+# ${repo.name}
+
+${repo.description}
+`;
+
+    fs.writeFileSync(path.join(__dirname, 'content/projects', `${repo.name}.mdx`), content);
+  });
 };
 
-export const Project = defineDocumentType(() => ({
-	name: "Project",
-	filePathPattern: "./projects/**/*.mdx",
-	contentType: "mdx",
-
-	fields: {
-		published: {
-			type: "boolean",
-		},
-		title: {
-			type: "string",
-			required: true,
-		},
-		description: {
-			type: "string",
-			required: true,
-		},
-		date: {
-			type: "date",
-		},
-		url: {
-			type: "string",
-		},
-		repository: {
-			type: "string",
-		},
-	},
-	computedFields,
-}));
-
-export const Page = defineDocumentType(() => ({
-	name: "Page",
-	filePathPattern: "pages/**/*.mdx",
-	contentType: "mdx",
-	fields: {
-		title: {
-			type: "string",
-			required: true,
-		},
-		description: {
-			type: "string",
-		},
-	},
-	computedFields,
-}));
-
-export default makeSource({
-	contentDirPath: "./content",
-	documentTypes: [Page, Project],
-	mdx: {
-		remarkPlugins: [remarkGfm],
-		rehypePlugins: [
-			rehypeSlug,
-			[
-				rehypePrettyCode,
-				{
-					theme: "github-dark",
-					onVisitLine(node) {
-						// Prevent lines from collapsing in `display: grid` mode, and allow empty
-						// lines to be copy/pasted
-						if (node.children.length === 0) {
-							node.children = [{ type: "text", value: " " }];
-						}
-					},
-					onVisitHighlightedLine(node) {
-						node.properties.className.push("line--highlighted");
-					},
-					onVisitHighlightedWord(node) {
-						node.properties.className = ["word--highlighted"];
-					},
-				},
-			],
-			[
-				rehypeAutolinkHeadings,
-				{
-					properties: {
-						className: ["subheading-anchor"],
-						ariaLabel: "Link to section",
-					},
-				},
-			],
-		],
-	},
-});
+fetchRepos().catch(console.error);
