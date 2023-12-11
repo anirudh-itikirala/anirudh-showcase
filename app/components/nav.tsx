09f8be1a1bb8792c6fb0a242f9a1dba0872a132f
aitikirala <aitikira@terpmail.umd.edu>
2023-12-11 05:05:58 -0500

    dark and light mode

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,22 +3,49 @@ import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
+
+
+
+  
+
 export const Navigation: React.FC = () => {
 	const ref = useRef<HTMLElement>(null);
 	const [isIntersecting, setIntersecting] = useState(true);
+	const [theme, setTheme] = useState('light'); // default theme
 
-	useEffect(() => {
+	const lightThemeStyles = {
+		backgroundColor: 'white',
+		color: 'black',
+		// Add other light theme styles here
+	  };
+	  
+	  const darkThemeStyles = {
+		backgroundColor: 'black',
+		color: 'white',
+		// Add other dark theme styles here
+	  };
+	  
+
+	  const toggleTheme = () => {
+		setTheme(prevTheme => prevTheme === 'light' ? 'dark' : 'light');
+	  };
+	
+	  useEffect(() => {
+		// Apply theme to body or a parent element if needed
+		// You can also apply it to individual components or divs
 		if (!ref.current) return;
 		const observer = new IntersectionObserver(([entry]) =>
-			setIntersecting(entry.isIntersecting),
+		  setIntersecting(entry.isIntersecting),
 		);
-
 		observer.observe(ref.current);
 		return () => observer.disconnect();
-	}, []);
+	  }, []);
 
 	return (
-		<header ref={ref}>
+		<header ref={ref} style={theme === 'light' ? lightThemeStyles : darkThemeStyles}>
+			<button onClick={toggleTheme} >
+      {theme === 'light' ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
+    </button>
 			<div
 				className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200 border-b  ${
 					isIntersecting
