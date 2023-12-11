6d9ccfb36b18f30c5c7f61f33042507bf51eb0cd
aitikirala <aitikira@terpmail.umd.edu>
2023-12-11 04:00:33 -0500

    name

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
             className="underline duration-500 hover:text-zinc-300"
           >
             unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+          </Link> Aarti Itikirala for developers.
         </h2>
       </div>
     </div>
