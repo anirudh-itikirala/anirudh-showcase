cedd816ad9a380761bcf056933e60ad46b374b76
aitikirala <aitikira@terpmail.umd.edu>
2023-12-20 15:51:52 -0500

    updated my working page

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -65,17 +65,38 @@ export default function Work() {
                   <br />
                   <h1 className='prose prose-xl mt-4 flex flex-col text-xl dark:prose-invert md:text-2xl'>
                     <div>SWE Intern</div>
-                    <div>May 2023 - August 2023</div>
+                    <div>July 2022 - Present</div>
                   </h1>
                 </div>
+
                 <div className='prose prose-lg w-5/6 pt-2 text-left tracking-tight dark:prose-invert md:prose-xl lg:prose-2xl md:w-2/5 md:pt-12'>
-                  <h6>
-                  Developed a Custom Github Connector: Leveraged AWS Lambda and App Flow to design and implement a custom Github connector, enabling seamless integration and automation of code repository workflows. Collaborated with cross-functional teams to ensure security, scalability, and efficiency.
+                  <h6>Developed a Custom Github Connector
+                    <div className='ml-6'>
+                    <li>
+                      Leveraged AWS Lambda and App Flow to design and implement a custom Github connector, enabling seamless integration and automation of code repository workflows. 
+                      </li>
+                      
+                      <li>
+                      Collaborated with cross-functional teams to ensure security, scalability, and efficiency.
+                      </li>
+                    </div>
                   </h6>
 
-                  <h6>
-                  Engineered a Workday Connector: Created a tailored Workday connector using AWS Lambda and App Flow, facilitating real-time data synchronization and automation between HR systems and internal applications. Led the process of testing, deployment, and ongoing support, contributing to improved operational efficiency
-                    </h6>
+                  <h6>Engineered a Workday Connector
+
+                  <div className='ml-6'>
+                      <li>
+                      Created a tailored Workday connector using AWS Lambda and App Flow, facilitating real-time data synchronization and automation between HR systems and internal applications. 
+                      </li>
+
+                      <li>
+                        Led the process of testing, deployment, and ongoing support, contributing to improved operational efficiency
+
+                      </li>
+                        
+                  </div>
+                  </h6>
+
                   <div className='flex w-full justify-end'>
                     <ArrowLink
                       className='text-sm'
@@ -84,21 +105,30 @@ export default function Work() {
                       Learn More
                     </ArrowLink>
                   </div>
+                  <div className='flex w-full justify-end'>
+                    <ArrowLink
+                      className='text-sm'
+                      href='https://www.linkedin.com/in/aarti-itikirala/details/recommendations/?detailScreenTabIndex=0'
+                    >
+                      Manager Recommendation
+                    </ArrowLink>
+                  </div>
                 </div>
               </div>
+                
               {/* UMD MATH COACH */}
               <div className='pane'>
                 <div className='w-5/6 pt-12 text-left md:w-2/5'>
                   <UnderlineLink
                     className='leading-1 prose text-left text-[2.5rem] font-semibold leading-10 dark:prose-invert sm:text-5xl'
-                    href='https://beta.sellerchain.com'
+                    href='https://www.linkedin.com/in/aarti-itikirala/details/recommendations/?detailScreenTabIndex=0'
                   >
                     University of Maryland
                   </UnderlineLink>
                   <br />
                   <h1 className='prose prose-xl mt-4 flex flex-col text-xl dark:prose-invert md:text-2xl'>
                     <div>Math Coach</div>
-                    <div>June 2022 - August 2022</div>
+                    <div> August 2021 - January 2022</div>
                   </h1>
                 </div>
                 <div className='prose prose-lg w-5/6 pt-2 text-left tracking-tight dark:prose-invert md:prose-xl lg:prose-2xl md:w-2/5 md:pt-12'>
@@ -115,130 +145,73 @@ export default function Work() {
 
                   <div className='ml-6'>
                       <li>
-                      : Assisted students with challenging homework problems, tailored test preparation strategies to individual learning styles, and offered personalized tutoring sessions, leading to improved academic performance and enhanced comprehension among participants.
+                      Assisted students with challenging homework problems, tailored test preparation strategies to individual learning styles, and offered personalized tutoring sessions, leading to improved academic performance and enhanced comprehension among participants.
                       </li>
                         
                   </div>
                   </h6>
-                  
+
                   <div className='flex w-full justify-end'>
                     <ArrowLink
                       className='text-sm'
-                      href='https://www.jasdeepahluwalia.com/files/sellerchain_recommendation'
+                      href='https://www.linkedin.com/in/aarti-itikirala/details/recommendations/?detailScreenTabIndex=0'
                     >
-                      CEO Recommendation
+                      Student Recommendation
                     </ArrowLink>
                   </div>
+
+                  <div className='flex w-full justify-end'>
+                    <ArrowLink
+                      className='text-sm'
+                      href='https://www.linkedin.com/in/aarti-itikirala/overlay/1635477243350/single-media-viewer/?profileId=ACoAADKaE60BFHBu2hKUmYKMm9keqRml1B5-UsE'
+                    >
+                      Recognition
+                    </ArrowLink>
+                  </div>
+
                 </div>
               </div>
-              {/* PROXYFARM PANE */}
+
+              
+
+
+              {/* Undergraduate Research Pane */}
               <div className='pane'>
                 <div className='w-5/6 pt-12 text-left md:w-2/5'>
                   <UnderlineLink
                     className='leading-1 prose text-left text-[2.5rem] font-semibold leading-10 dark:prose-invert sm:text-5xl'
-                    href='https://proxy-farm.com'
+                    href='https://www.linkedin.com/in/aarti-itikirala/details/recommendations/?detailScreenTabIndex=0'
                   >
-                    Proxyfarm
+                    University of Maryland
                   </UnderlineLink>
                   <br />
                   <h1 className='prose prose-xl mt-2 flex flex-col text-xl dark:prose-invert md:text-2xl'>
-                    <div>Founding Engineer</div>
-                    <div>January 2021 - August 2021</div>
+                    <div>Undergraduate Research Assistant</div>
+                    <div>November 2020 - February 2021</div>
                   </h1>
                 </div>
                 <div className='prose prose-lg w-5/6 pt-2 text-left tracking-tight dark:prose-invert md:prose-xl lg:prose-2xl md:w-2/5 md:pt-12'>
                   <h6>
-                    Founded and developed a startup offering a scalable and
-                    high-performance proxy service for web scraping, SEO tools,
-                    and secure browsing.
-                    <div className='ml-6'>
-                      <li>
-                        Developed a scalable backend for a proxy service
-                        handling 10,000+ IPs, serving 100+ users monthly.
-                      </li>
-                      <li>
-                        Processed high-volume traffic with Redis & REST APIs;
-                        used Bash scripts to concurrently validate user limits.
-                      </li>
-                      <li>
-                        Engineered a user & employee dashboard with NodeJS,
-                        Websockets, MongoDB, Oauth2, HTML, CSS, & Chart.js.,
-                        allowing employees to validate orders and set stock, and
-                        clients to manage their subscriptions and access their
-                        proxy credentials.
-                      </li>
-                      <li>
-                        Achieved a 99.9% system uptime, displaying expertise in
-                        high-reliability & optimizing resource allocation.
-                      </li>
-                      <li>
-                        Managed business aspects, including marketing, sales,
-                        and customer support, leading to a growing client base
-                        and positive feedback
-                      </li>
-                    </div>
+                  Collected, coded, and prepared state-level COVID-19 as well as political affiliation data
                   </h6>
-                  <div className='flex w-full justify-end'>
-                    <ArrowLink
-                      className='text-sm'
-                      href='https://www.jasdeepahluwalia.com/experience/proxyfarm'
-                    >
-                      Learn More
-                    </ArrowLink>
-                  </div>
-                </div>
-              </div>
-              {/* THE BOT TRADE PANE */}
-              <div className='pane'>
-                <div className='w-5/6 pt-12 text-left md:w-2/5'>
-                  <UnderlineLink
-                    className='leading-1 prose text-left text-[2.5rem] font-semibold leading-10 dark:prose-invert max-[393px]:w-min sm:text-5xl'
-                    href='https://twitter.com/TheBotTrade'
-                  >
-                    The Bot Trade
-                  </UnderlineLink>
-                  <br />
-                  <h1 className='prose prose-xl mt-2 flex flex-col text-xl dark:prose-invert md:text-3xl'>
-                    <div>Administrator</div>
-                    <div>January 2021 - September 2021</div>
-                  </h1>
-                </div>
-                <div className='prose prose-lg w-5/6 pt-2 text-left tracking-tight dark:prose-invert md:prose-xl lg:prose-2xl md:w-2/5 md:pt-12'>
                   <h6>
-                    Consulted for a leading software trading and cryptocurrency
-                    advisory firm, providing expert guidance to clients on
-                    investment strategies, market trends, and risk management.
-                    <div className='ml-6'>
-                      <li>
-                        Assessed client needs and delivered strategic investment
-                        recommendations for development company ventures,
-                        contributing to the success and growth of multiple
-                        startups.
-                      </li>
-                      <li>
-                        Directed a high-performing team of 10+ employees,
-                        optimizing payroll and monitoring productivity to
-                        achieve maximum efficiency and ensure consistent
-                        delivery of expert advice to clients.
-                      </li>
-                      <li>
-                        Established relationships with creators to create
-                        partnerships, driving member engagement and fostering
-                        long-term business growth through collaboration and
-                        resource sharing.
-                      </li>
-                    </div>
+                  Verified user-reported Twitter data using online research
+                  </h6>
+
+                  <h6>
+                  Analyzed Twitter data to understand the spatiotemporal spread of misinformation regarding mask use
                   </h6>
                   <div className='flex w-full justify-end'>
                     <ArrowLink
                       className='text-sm'
-                      href='https://twitter.com/TheBotTrade'
+                      href='https://www.linkedin.com/in/aarti-itikirala/details/recommendations/?detailScreenTabIndex=0'
                     >
-                      Learn More
+                      Recommendation from Professor Erdemandi
                     </ArrowLink>
                   </div>
                 </div>
               </div>
+              
             </div>
             <Footer />
           </div>
