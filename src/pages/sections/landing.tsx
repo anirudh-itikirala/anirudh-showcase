e090100897df3a1c1e469981db2a3e9d28de6b1e
aitikirala <aitikira@terpmail.umd.edu>
2023-12-20 23:50:39 -0500

    updated general style touches

--- src/pages/sections/landing.tsx
+++ src/pages/sections/landing.tsx
@@ -19,7 +19,7 @@ export default function Landing() {
           mouseControls: true,
           touchControls: true,
           gyroControls: false,
-          color: 0xADD8E6,
+          color: 0xd27d2d,
           points: 9.0,
           maxDistance: 15.0,
           spacing: 20.0,
@@ -33,6 +33,7 @@ export default function Landing() {
     handleMenuClick();
   }, [vantaEffect]);
 
+  // eslint-disable-next-line unused-imports/no-unused-vars
   const [popCard, setPopCard] = React.useState('hidden');
   const [fade, setFade] = React.useState(false);
 
@@ -40,6 +41,36 @@ export default function Landing() {
     setPopCard('inline-block');
     setFade(true);
   };
+  //
+  // async function getRepoClones(): Promise<number> {
+  //   const url =
+  //     'https://api.github.com/repos/ahluwalij/jasdeep-showcase/traffic/clones';
+  //   const headers = new Headers({
+  //     Accept: 'application/vnd.github+json',
+  //     Authorization: `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+  //     'X-GitHub-Api-Version': '2022-11-28',
+  //   });
+
+  //   try {
+  //     const response = await fetch(url, { method: 'GET', headers });
+
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! status: ${response.status}`);
+  //     }
+
+  //     const data = await response.json();
+  //     return data.count;
+  //   } catch (error) {
+  //     // console.error('Error fetching repository clones:', error);
+  //     return 0;
+  //   }
+  // }
+
+  // getRepoClones().then((clones) => {
+  //   const clonesEl = document.getElementById('clones');
+  //   if (clonesEl != null) clonesEl.innerHTML = clones.toString();
+  //   // console.log(`Number of clones: ${clones}`);
+  // });
 
   return (
     <>
@@ -57,7 +88,7 @@ export default function Landing() {
               className='relative block translate-y-12 transform text-6xl font-black transition-all duration-500 ease-out'
               data-replace='{ "translate-y-12": "translate-y-0" }'
             >
-              <h1 className='flex min-h-[192px] flex-col items-start font-bold tracking-normal text-primary dark:text-[#bbecf7] max-[640px]:text-6xl max-[400px]:text-5xl sm:text-7xl md:text-8xl'>
+              <h1 className='flex min-h-[192px] flex-col items-start font-bold tracking-normal text-primary dark:text-[#FAD5A5] max-[640px]:text-6xl max-[400px]:text-5xl sm:text-7xl md:text-8xl'>
                 <div>Aarti</div>
                 <div>Itikirala</div>
                 <div className='text-md flex flex-col items-start gap-1 text-left md:text-lg'>
@@ -80,11 +111,30 @@ export default function Landing() {
                       </ArrowLink>
                     </h3>
                   </div>
-                  {}
+                  {/* <h3 className='flex flex-wrap gap-1'>
+                    With
+                    <div
+                      id='clones'
+                      className='font-bold text-slate-700 dark:text-gray-300'
+                    >
+                      ...
+                    </div>
+                    clones and
+                    <div
+                      id='views'
+                      className='font-bold text-slate-700 dark:text-gray-300'
+                    >
+                      ...
+                    </div>
+                    views in the last 14 days
+                  </h3> */}
                 </div>
               </h1>
 
-              {}
+              {/* <h2 className='prose flex max-w-screen-sm items-start text-left text-3xl font-semibold leading-10 tracking-wide dark:prose-invert max-[768px]:mt-60 max-[700px]:mt-52 max-[500px]:mt-44 max-[436px]:mt-32 max-[390px]:mt-12 sm:text-4xl md:mt-72 xl:mt-[27.5rem]'>
+                Fullstack Developer with a passion for developing amazing things
+                for amazing people.
+              </h2> */}
             </div>
 
             <div className='mt-auto flex w-full justify-center'>
