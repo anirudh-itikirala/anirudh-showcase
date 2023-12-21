869011a2bd3aa9e2fb633833d083b051d97cd9b1
aitikirala <aitikira@terpmail.umd.edu>
2023-12-20 22:59:17 -0500

    updated IEFS part

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -226,139 +226,104 @@ export default function Education() {
                 </div>
               </div>
 
-              {/* I4C Student Advisory Board */}
+              {/* IEFS */}
               <div className="pane">
                 <div className="w-5/6 pt-12 text-left md:w-2/5">
-                  <h1 className="leading-1 prose text-left text-[2.5rem] font-semibold leading-10 dark:prose-invert min-[470px]:hidden sm:text-5xl">
-                    I4C Student Advisory Board
-                  </h1>
-                  <h1 className="leading-1 prose hidden text-left text-[2.5rem] font-semibold leading-10 dark:prose-invert min-[470px]:inline sm:text-5xl">
-                    I4C Student Advisory Board
-                  </h1>
+                  <UnderlineLink
+                    className="leading-1 prose w-fit text-left text-[2.5rem] font-semibold leading-10 dark:prose-invert sm:text-5xl xl:hidden"
+                    href="https://www.cs.umd.edu/"
+                  >
+                    IEFS
+                  </UnderlineLink>
+                  <UnderlineLink
+                    className="leading-1 prose hidden w-fit text-left text-[2.5rem] font-semibold leading-10 dark:prose-invert sm:text-5xl xl:inline"
+                    href="https://www.cs.umd.edu/"
+                  >
+                    International Economics and Finance Society
+                  </UnderlineLink>
                   <br />
-                  <h1 className="prose prose-xl mt-2 text-xl dark:prose-invert md:text-3xl">
-                    August 2020 - May 2021
+                  <h1 className="prose prose-xl mt-4 flex flex-col text-xl dark:prose-invert md:text-3xl">
+                    <div>Content Creation Lead</div>
+                    <div>August 2020 - May 2021</div>
                   </h1>
                 </div>
                 <div className="prose prose-lg w-5/6 pt-2 text-left tracking-tight dark:prose-invert md:prose-xl lg:prose-2xl md:w-2/5 md:pt-12">
-                  <h6>
-                    Engineered a CNN to detect/distinguish between malicious
-                    malaria strains/cells in images using Python, Tensorflow,
-                    Keras, Seaborn, Neural Networks, Computer Vision, and Google
-                    Colab.
+                  <h6> Research and analyze global economic trends and financial
+                    news, providing insightful commentary on current affairs
+                    both domestically and internationally.
                     <div className="ml-6">
                       <li>
-                        Trained neural network against a dataset of 100,000
-                        archived images from compiled research.
+                      Contribute to the society's mission by preparing
+                        presentations and reports that drive conversations on
+                        economic stability and growth prospects.
                       </li>
                       <li>
-                        Collaborated with a multidisciplinary team to optimize
-                        the CNN's performance and accuracy, achieving a 85%
-                        success rate in identifying malaria strains.
+                      Publish well-crafted articles on Medium, elucidating
+                        complex economic concepts and their real-world
+                        implications for a diverse readership.
                       </li>
                       <li>
-                        Presented research findings to faculty and fellow
-                        students, sparking interest in further development and
-                        collaboration.
+                        Collaborate with club members to facilitate discussions
+                        on international trade, monetary policy, and market
+                        dynamics, enhancing collective understanding.
                       </li>
                     </div>
                   </h6>
-                  {}
-                </div>
-                {/* International Economics and Finance Society*/}
-                <div className="pane">
-                  <div className="w-5/6 pt-12 text-left md:w-2/5">
-                    <h1 className="leading-1 prose text-left text-[2.5rem] font-semibold leading-10 dark:prose-invert min-[470px]:hidden sm:text-5xl">
-                      BigTh!nk AI
-                    </h1>
-                    <h1 className="leading-1 prose hidden text-left text-[2.5rem] font-semibold leading-10 dark:prose-invert min-[470px]:inline sm:text-5xl">
-                      BigTh!nk AI Club
-                    </h1>
-                    <br />
-                    <h1 className="prose prose-xl mt-2 text-xl dark:prose-invert md:text-3xl">
-                      August 2022 - Current
-                    </h1>
-                  </div>
-                  <div className="prose prose-lg w-5/6 pt-2 text-left tracking-tight dark:prose-invert md:prose-xl lg:prose-2xl md:w-2/5 md:pt-12">
-                    <h6>
-                      Research and analyze global economic trends and financial
-                      news, providing insightful commentary on current affairs
-                      both domestically and internationally.
-                      <div className="ml-6">
-                        <li>
-                          Contribute to the society's mission by preparing
-                          presentations and reports that drive conversations on
-                          economic stability and growth prospects.
-                        </li>
-                        <li>
-                          Publish well-crafted articles on Medium, elucidating
-                          complex economic concepts and their real-world
-                          implications for a diverse readership.
-                        </li>
-                        <li>
-                          Collaborate with club members to facilitate
-                          discussions on international trade, monetary policy,
-                          and market dynamics, enhancing collective
-                          understanding.
-                        </li>
-                      </div>
-                    </h6>
-                    {}
-                  </div>
 
                   <div className="flex w-full justify-end">
                     <ArrowLink
                       className="text-sm"
-                      href=" https://medium.com/umd-iefs-online-content/economics-of-vaccine-development-382b330c93b"
+                      href="https://medium.com/umd-iefs-online-content/economics-of-vaccine-development-382b330c93b"
                     >
                       Learn More
                     </ArrowLink>
                   </div>
                 </div>
-                {/* WMUC */}
-                <div className="pane">
-                  <div className="w-5/6 pt-12 text-left md:w-2/5">
-                    <UnderlineLink
-                      className="leading-1 prose w-min text-left text-[1.8rem] font-semibold leading-10 dark:prose-invert sm:text-4xl md:text-5xl lg:hidden"
-                      href="https://www.wmuc.umd.edu"
-                    >
-                      Johns Hopkins BMEI
-                    </UnderlineLink>
-                    <UnderlineLink
-                      className="leading-1 prose hidden w-min text-[2.5rem] font-semibold leading-10 dark:prose-invert sm:text-5xl lg:block"
-                      href="https://ei.jhu.edu/programs/bmei-online/"
-                    >
-                      Johns Hopkins BioEngineering
-                    </UnderlineLink>
-                    <br />
-                    <h1 className="prose prose-xl mt-2 text-xl dark:prose-invert md:text-3xl">
-                      Sept 2020 - Jan 2021
-                    </h1>
-                  </div>
-                  <div className="prose prose-lg w-5/6 pt-2 text-left tracking-tight dark:prose-invert md:prose-xl lg:prose-2xl md:w-2/5 md:pt-12">
-                    <h6>
-                      Modeled systems using engineering principles to solve
-                      biological, physiological, and/or medical design problems
-                      over a six-week period.
-                      <div className="ml-6">
-                        <li>
-                          Developed a prototype for a wearable device that
-                          monitors vital signs and alerts healthcare providers
-                          of any anomalies, demonstrating a deep understanding
-                          of both hardware and software design principles.
-                        </li>
-                        <li>
-                          Collaborated with a team of engineers and medical
-                          professionals to ensure the prototype's functionality
-                          and accuracy in real-world situations.
-                        </li>
-                        <li>
-                          Presented project outcomes to a panel of experts and
-                          received constructive feedback for further improvement
-                        </li>
-                      </div>
-                    </h6>
-                  </div>
+              </div>
+
+              {/* WMUC */}
+              <div className="pane">
+                <div className="w-5/6 pt-12 text-left md:w-2/5">
+                  <UnderlineLink
+                    className="leading-1 prose w-min text-left text-[1.8rem] font-semibold leading-10 dark:prose-invert sm:text-4xl md:text-5xl lg:hidden"
+                    href="https://www.wmuc.umd.edu"
+                  >
+                    Johns Hopkins BMEI
+                  </UnderlineLink>
+                  <UnderlineLink
+                    className="leading-1 prose hidden w-min text-[2.5rem] font-semibold leading-10 dark:prose-invert sm:text-5xl lg:block"
+                    href="https://ei.jhu.edu/programs/bmei-online/"
+                  >
+                    Johns Hopkins BioEngineering
+                  </UnderlineLink>
+                  <br />
+                  <h1 className="prose prose-xl mt-2 text-xl dark:prose-invert md:text-3xl">
+                    Sept 2020 - Jan 2021
+                  </h1>
+                </div>
+                <div className="prose prose-lg w-5/6 pt-2 text-left tracking-tight dark:prose-invert md:prose-xl lg:prose-2xl md:w-2/5 md:pt-12">
+                  <h6>
+                    Modeled systems using engineering principles to solve
+                    biological, physiological, and/or medical design problems
+                    over a six-week period.
+                    <div className="ml-6">
+                      <li>
+                        Developed a prototype for a wearable device that
+                        monitors vital signs and alerts healthcare providers of
+                        any anomalies, demonstrating a deep understanding of
+                        both hardware and software design principles.
+                      </li>
+                      <li>
+                        Collaborated with a team of engineers and medical
+                        professionals to ensure the prototype's functionality
+                        and accuracy in real-world situations.
+                      </li>
+                      <li>
+                        Presented project outcomes to a panel of experts and
+                        received constructive feedback for further improvement
+                      </li>
+                    </div>
+                  </h6>
                 </div>
               </div>
             </div>
